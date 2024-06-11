433235a49a369cf9a69fd08644bfa915747f340f
nika <165318260+nika2607@users.noreply.github.com>
2024-06-11 23:49:05 +0400

    Add files via upload

--- რაღაცა/day16/home work/yddhf.py
+++ რაღაცა/day16/home work/yddhf.py
@@ -1,3 +1,7 @@
-name=input(("enter your name:"))
-s_name=(("entter yourr namme:"))
-age=input(("enter your age:"))
+name=input(("enter your name:  "))
+s_name=(("entter yourr namme: "))
+age=input(("enter your age: "))
+p_name=input(("entter your parent's name: "))
+p_s_name=input(("ennret yoyur parants s_name: "))
+
+print( name," ",s_name," ",age," ",p_name," ",p_s_name)
