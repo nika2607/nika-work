e87ef793d69a97b90a7c8c9052bc0cf8f8270dad
nika <165318260+nika2607@users.noreply.github.com>
2024-07-05 11:06:00 +0400

    Add files via upload

---
+++ რაღაცა/just for fun/swert.py
@@ -0,0 +1,8 @@
+tetres_y="1999"
+gta6_y="2025"
+first_game_y="1"
+my_last_game_y="2124"
+my_first_game_y="2034"
+your_first_game_y=input("enter your first games year : ")
+qwa="-my _ yours-"
+print(my_first_game_y  +  qwa +  your_first_game_y)
